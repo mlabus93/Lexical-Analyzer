ccd620aae03942ca76f8f914f58c51ab69dae8c6
mlabus93 <michaellabus@Michaels-MacBook-Pro.local>
2015-06-08 14:12:55 -0400

    Initial Commit

---
+++ LexicalAnalyzer/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  LexicalAnalyzer
+//
+//  Created by Michael Labus on 6/8/15.
+//  Copyright (c) 2015 Michael Labus. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
